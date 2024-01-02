221756dc6ad8fc30cd82b5cc8bf4e1c9a0f3889b
Piotr Gaszczynski <piotrek.gaszczynski@gmail.com>
2024-01-02 21:21:40 +0100

    Add main.py and basic calculator functions

--- main.py
+++ main.py
@@ -5,51 +5,45 @@ Any comments welcome :)
 """
 
 def main():
-    
-    print("\n--------Welcome--------\n"
-          "---Simple-calculator---\n"
-          "---------------pedro-v3")
 
     print_info()
 
     while count_again():
 
-        user_string = input("Enter operation: ")
-        list = user_string.split()
+        user_input_string = input("Enter operation: ")
+        user_input_list = user_input_string.split()
 
-        number1 = convert_input_to_float(list[0])
-        number2 = convert_input_to_float(list[2])
-        operation = list[1]
+        number1 = convert_input_to_float(user_input_list[0])
+        number2 = convert_input_to_float(user_input_list[2])
+        operation = user_input_list[1]
 
-        if check_input_type(number1) or check_input_type(number2):
+        if is_string(number1) or is_string(number2):
             continue
 
-        number1 = float(number1)
-        number2 = float(number2)
-        operation = str(operation)
-
-        print(f"Result: {calculate(number1, operation, number2)}\n")
+        result = calculate(float(number1), 
+                           str(operation), 
+                           float(number2))
+        
+        print(f"Result: {result}\n")
 
 
 def count_again():
-    """Checks if user wants to count or quit. Returns True or False"""
+    """While loop checking if user wants to count (returns True) or quit (returns False)"""
     while True:
         answer = input("Continue? Enter y to count or q to quit? ")
         answer = answer.strip().lower()
         if answer == "q" or answer == "quit":
             print("Ok. Quit")
             return False
-            break
         elif answer == "y" or answer == "yes":
             print("\nOk. Lets count.\n")
             return True
-            break
         else:
             print("Wrong answer.")
 
 
 def calculate(number1, operation, number2):
-    """Making a calculation ,depending on operation input. Returns result"""
+    """Making a calculation, depending on operation input. Returns result"""
     if operation == "+":
         result = number1 + number2
     elif operation == "-":
@@ -66,7 +60,7 @@ def calculate(number1, operation, number2):
     return result
 
 
-def check_input_type(input):
+def is_string(input):
     """Checks if input is float, int or string."""
     try:
         input = float(input)
@@ -84,19 +78,19 @@ def convert_input_to_float(input):
     """Converts entered decimals with ',' to floats with '.' if possible."""
     if ',' in input:
         splitted = input.split(',')
-
         if len(splitted) == 2:
-            converted = (splitted[0]+'.'+splitted[1])
-            converted = float(converted)
+            converted = float(splitted[0]+'.'+splitted[1])
             return converted
-
+        else:
+            return input
     else:
         return input
 
 
 def print_info():
     """Prints simple info of supported operations."""
-    print("\nEnter first number, operation and second number."
+    print("\n--- calculator.py ---"
+          "\nEnter first number, operation and second number."
           "\ndivided by space (for example 2 + 3)"
           "\nsupperted operations: + - / * %\n")
 
